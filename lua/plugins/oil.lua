41ce887eb440f3dc19cda1123f0edae8aed6347d
Thorkil Holm-Jacobsen <thorkil@because.eco>
2025-03-11 14:27:32 +0100

    added oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    config = function()
+      require("oil").setup()
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end,
+  },
+}
