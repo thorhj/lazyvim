6ca752014937de1e05eed8a6beb7a6e7ad278f09
Thorkil Holm-Jacobsen <burpen@gmail.com>
2025-03-18 21:05:20 +0100

    added vimade to dim inactive windows

---
+++ lua/plugins/vimade.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "TaDaa/vimade",
+    event = "VeryLazy",
+    opts = {
+      recipe = { "minimalist", { animate = true } },
+      fadelevel = 0.6,
+      ncmode = "windows",
+    },
+  },
+}
